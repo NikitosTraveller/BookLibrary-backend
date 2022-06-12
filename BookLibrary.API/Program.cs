2bcec74ae49ba85dfb7fd6bef8702d47621d6123
Nikita Semchenko <nikita.semchenko@effective-soft.com>
2022-06-12 23:45:36 +0300

    remove autofac

--- BookLibrary.API/Program.cs
+++ BookLibrary.API/Program.cs
@@ -8,11 +8,11 @@ using BookLibrary.API.Mapping;
 using FluentValidation.AspNetCore;
 using BookLibrary.DAL;
 using BookLibrary.DAL.DataWorkers;
-using Autofac.Extensions.DependencyInjection;
 using BookLibrary.BL;
 using Autofac;
 using BookLibrary.DAL.Contracts;
 using BookLibrary.API;
+using BookLibrary.DAL.DataHelpers;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -33,14 +33,14 @@ builder.Services.AddSingleton(mapper);
 
 builder.Services.AddControllers();
 
-builder.Services.AddAutofac();
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddFluentValidation();
 
-builder.Services.AddScoped(typeof(IFinder<>), typeof(Finder<>));
+builder.Services.AddScoped<IBookFinder, BookFinder>();
+builder.Services.AddScoped<IUserFinder, UserFinder>();
+builder.Services.AddScoped<ICommentFinder, CommentFinder>();
 builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
@@ -48,13 +48,6 @@ builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped<IBookService, BookService>();
 builder.Services.AddScoped<ICommentService, CommentService>();
 builder.Services.AddScoped<IUserService, UserService>();
-
-builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
-
-builder.Host.ConfigureContainer<ContainerBuilder>(builder =>
-{
-    builder.RegisterModule(new RegisterModules());
-});
 
 var app = builder.Build();
 
